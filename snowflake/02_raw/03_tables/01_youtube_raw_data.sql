9473b817f623233fee6b562bae56d0c230bc8c8f
Matu <agostonmate9@gmail.com>
2026-05-22 10:20:21 +0200

    video extend

---
+++ snowflake/02_raw/03_tables/01_youtube_raw_data.sql
@@ -0,0 +1,7 @@
+-- 1. Create Persistent Raw Table for YouTube API Raw Data
+-- Resides in RAW schema, meant for persistent historical storage of raw JSON.
+
+CREATE OR REPLACE TABLE RAW.YOUTUBE_RAW_DATA (
+    RAW_JSON VARIANT,
+    EXTRACTED_AT TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP()
+);
