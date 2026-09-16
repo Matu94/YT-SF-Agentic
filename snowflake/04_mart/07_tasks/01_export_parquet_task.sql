2686038f97a472b2f31e1d9b117ee7c10e7758eb
Matu <agostonmate9@gmail.com>
2026-09-16 19:03:30 +0200

    refine sql

--- snowflake/04_mart/07_tasks/01_export_parquet_task.sql
+++ snowflake/04_mart/07_tasks/01_export_parquet_task.sql
@@ -2,12 +2,7 @@
 -- 01_export_parquet_task.sql
 -- Task to schedule the daily export of Parquet files to S3
 -- ==============================================================
-
-USE ROLE {{SNOWFLAKE_DATABASE}}_TRANSFORM_ROLE;
-USE DATABASE {{SNOWFLAKE_DATABASE}};
-USE SCHEMA MART;
-
-CREATE OR REPLACE TASK EXPORT_MART_TO_S3_TASK
+CREATE OR REPLACE TASK MART.EXPORT_MART_TO_S3_TASK
   WAREHOUSE = YT_SF_TRANSFORM_WH
   SCHEDULE = 'USING CRON 30 2 * * * Europe/Budapest'
   COMMENT = 'Daily task to export presentation views to S3 as Parquet'
